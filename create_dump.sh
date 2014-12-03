2659ae15fead109591155e914e788c0ca10a36cf
AnnKpx Server <annkpx@gmail.com>
2014-12-03 18:26:28 +0300

    dump 2014-12-03

---
+++ create_dump.sh
@@ -0,0 +1,8 @@
+#! /bin/bash
+
+DATE=`date +%Y-%m-%d`
+python manage.py dumpdata call_request catalog feedback news pages shop slideshow auth users --natural > dump_"$DATE".json
+git add -A
+git commit -a -m "dump ""$DATE"
+git push
+
